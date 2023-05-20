ff8813c791dfe5bf4df0fe5a90f6ca4e34e8cf7b
rosmaidalestarini5@gmail.com <134145623+rosmaidalestarini5@users.noreply.github.com>
2023-05-21 00:20:09 +0700

    Add files via upload

--- writeOnlyError.js
+++ writeOnlyError.js
@@ -1,3 +1,4 @@
-export default function _writeOnlyError(name) {
+function _writeOnlyError(name) {
   throw new TypeError("\"" + name + "\" is write-only");
 }
+module.exports = _writeOnlyError, module.exports.__esModule = true, module.exports["default"] = module.exports;
